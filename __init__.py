a660e77a9c69dae7ecfb1cf3bacf4e5c896341a2
cjohnsoncruz <49743933+cjohnsoncruz@users.noreply.github.com>
2025-02-08 15:40:55 -0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,6 @@
-
-# Import objects
-
-# Capture the original matplotlib rcParams
-import matplotlib as mpl
-_orig_rc_params = mpl.rcParams.copy()
+"""
+Module for posthoc test plotting functions with matplotlib and seaborn.
+"""
 
 # Define the plot_posthoc_test version
 __version__ = "0.0.1"
