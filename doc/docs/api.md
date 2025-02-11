42ec27ca570ac0f5c0efe37eb6ae4cf3ff9ed342
cjohnsoncruz <49743933+cjohnsoncruz@users.noreply.github.com>
2025-02-11 13:18:50 -0800

    fixed mkdocstring import
    + added importing of API docstrings
    +added license
    +build and deploy docs

---
+++ doc/docs/api.md
@@ -0,0 +1,3 @@
+# API Documentation
+
+::: plot_posthoc_test.plot_stat_annotate
--- setup.py
+++ setup.py
@@ -4,15 +4,16 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="plot_posthoc_test-pkg-your-username",
+    name="plot_posthoc_test",
     version="0.0.1",
     author="Carlos Johnson-Cruz",
     author_email="cjohnsoncruz@gmail.com",
     description="Package for performing and plotting posthoc tests, for use with seaborn/matplotlib",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(),
+    url="https://github.com/cjohnsoncruz/plot_posthoc_test",
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
